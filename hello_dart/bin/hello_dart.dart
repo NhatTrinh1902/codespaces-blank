7eeca1e4f553e6e1d8cdcf9773b6613dba426512
Trịnh Minh Nhật <trinhminhnhat1922004@gmail.com>
2026-09-12 01:51:59 +0000

    đồng bộ 12/9

---
+++ hello_dart/bin/hello_dart.dart
@@ -0,0 +1,9 @@
+import 'package:hello_dart/hello_dart.dart' as hello_dart;
+
+void main() {
+final ten = 'Sinh vien';
+print('Xin chao, $ten! Day la chuong trinh Dart dau tien.');
+for (var i = 1; i <= 5; i++) {
+print('Dem: $i');
+}
+}
